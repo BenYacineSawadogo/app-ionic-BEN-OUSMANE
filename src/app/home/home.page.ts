a24a34f0d8228f057953e9f349527a7ab5af9ba2
BenYacineSawadogo <f.benyacine27@gmail.com>
2022-12-27 23:41:40 +0000

    Add existing project files to Git

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Note } from './note.model';
+import { Storage } from '@ionic/storage';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  newNote: Note = new Note(0, '', '');
+  notes: Note[] = [];
+  editing = false;
+  editedNote:Note = { score: 0, course: "", semester: "" };
 
-  constructor() {}
+  semesters = ["S1", "S2", "S3", "S4", "S5", "S6","S7","S8","S9","S10"];
 
-}
+
+  constructor(private nativeStorage: NativeStorage) {}
+
+  addNote() {
+    this.notes.push(new Note(this.newNote.score, this.newNote.course, this.newNote.semester));
+    this.newNote = new Note(0, '', '');
+  }
+  deleteNote(note: Note) {
+    const index = this.notes.indexOf(note);
+    this.notes.splice(index, 1);
+  }
+  editNote(note: Note) {
+
+    this.editing = true;
+
+    const index = this.notes.indexOf(note);
+    this.notes[index] = { ...note };
+    this.editedNote=note
+    }
+  cancelEdit() {
+    this.editing = false;
+    this.newNote = new Note(0, '', '');
+  }
+
+  saveEdit() {
+    const index = this.notes.findIndex(n => n.course === this.editedNote.course);
+    this.notes[index] = this.editedNote;
+    this.editing = false;
+    this.editedNote = { score: 0, course: "", semester: ""};
+  }
+
+
+  getAverage(notes: Note[] = this.notes) {
+    if (notes.length === 0) {
+      return 0;
+    }
+    return notes.reduce((acc, note) => acc + note.score, 0) / notes.length;
+  }
+
+  getSemesterAverage(semester: string) {
+    const semesterNotes = this.notes.filter(note => note.semester === semester);
+    return this.getAverage(semesterNotes);
+  }
+
+  }
+
---
+++ src/app/home/note.model.ts
@@ -0,0 +1,13 @@
+export class Note {
+  score: number;
+  course: string;
+  semester: string;
+
+  constructor(score: number, course: string, semester: string) {
+    this.score = score;
+    this.course = course;
+    this.semester = semester;
+  }
+
+
+}
