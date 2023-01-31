b9d89c8b26e9983fac78ec159a773b96d4bf95a8
BenYacineSawadogo <f.benyacine27@gmail.com>
2023-01-31 21:47:55 +0000

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,7 +21,7 @@ export class HomePage {
   semesters = ["S1", "S2", "S3", "S4", "S5", "S6","S7","S8","S9","S10"];
 
 
-  constructor(private nativeStorage: NativeStorage) {}
+  constructor() {}
 
   addNote() {
     this.notes.push(new Note(this.newNote.score, this.newNote.course, this.newNote.semester));
